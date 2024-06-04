702a82fe6477c8cb6bd3293632a3abb3d5846bc0
bekhruz <ergashef1@gmail.com>
2024-06-04 14:35:25 +0300

    javasc_hw2

---
+++ script.js
@@ -0,0 +1,12 @@
+var son = +prompt('Yoshingizni kiriting')
+if(0<=150)
+if(son <=18){
+    alert(' hali yoshsiz oqishingiz kerak ' )}
+    else if(son <=50 ){
+    alert(' siz ishlashingiz kerak ' )}
+    else if(son <=59 ){ 
+    alert(' siz pensiyaga yaqinsiz')}
+    else if(son <=150 ){
+    alert('siz tirik bolsangiz nafaqadan zavqlaning')}
+    else{
+    alert(' Qandaydir hatolik bor ')}
